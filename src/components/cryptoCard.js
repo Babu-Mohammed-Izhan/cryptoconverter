6550b586e940f840ba7a16c3fd31066615b349b8
Izhan <babuizhan1234@gmail.com>
2022-02-10 21:53:59 +0530

    Added news api and fixed exchange

--- src/components/cryptoCard.js
+++ src/components/cryptoCard.js
@@ -1,6 +1,10 @@
-const Card = ({ cryptData }) => {
+const Card = ({ cryptData, green, red }) => {
   return (
-    <div className="flex items-center justify-center flex-col bg-purple-200 w-36 h-36 mx-auto rounded">
+    <div
+      className={` ${red ? 'border-4 border-red-500' : ''} ${
+        green ? 'border-4 border-green-500' : ''
+      } flex items-center justify-center flex-col bg-purple-200 w-36 h-36 mx-auto rounded cursor-pointer`}
+    >
       <h1 className="text-black font-bold text-center">{cryptData.symbol}</h1>
       <h2 className="text-black mt-2 text-center">{cryptData.name}</h2>
     </div>
--- src/components/news.js
+++ src/components/news.js
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import NewsCard from './newsCard';
+
 const News = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const getNews = async () => {
+      const res = await axios.get(
+        'https://newsdata.io/api/1/news?apikey=pub_4477531bf3845c78f495be527da0375acb6a&category=business&q=crypto'
+      );
+      setNews(res.data.results);
+    };
+    getNews();
+  }, []);
   return (
-    <div>
-      <div></div>
-    </div>
+    <section className="flex items-center justify-center flex-col">
+      <h1 className="text-4xl font-bold">News</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10 mx-10">
+        {news.map((n) => {
+          return <NewsCard key={n.title} data={n} />;
+        })}
+      </div>
+    </section>
   );
 };
 
